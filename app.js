2cc296391e09f2fcf9813d526f6c0f8bdc7031ce
Hoai Viet Nguyen <nguyen@hoaiviet.de>
2021-01-24 19:44:01 +0100

    add headers logging

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/users', usersRouter);
 
 app.get('*', function(req, res) {
+	console.log(req.headers);
 	res.sendFile("static/index.html",{ root : __dirname});
 });
 
